30f06d4d1f134942c92cfe612e54167268eb3012
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2025-07-12 09:36:26 +0200

    development dbt files

--- dbt/barca_project/models/marts/fact/fct_player_season_stats.sql
+++ dbt/barca_project/models/marts/fact/fct_player_season_stats.sql
@@ -41,6 +41,34 @@ SELECT
   CASE 
     WHEN shots::float > 0 THEN "xG"::float / shots::float
     ELSE NULL
-  END AS xG_per_shot                         -- xG / shots
+  END AS xG_per_shot,                         -- xG / shots
+
+  CASE WHEN time > 0 THEN goals * 90.0 / time ELSE NULL END AS goals_per_90,
+  CASE WHEN time > 0 THEN assists * 90.0 / time ELSE NULL END AS assists_per_90,
+  CASE WHEN time > 0 THEN shots * 90.0 / time ELSE NULL END AS shots_per_90,
+  CASE WHEN time > 0 THEN "xG" * 90.0 / time ELSE NULL END AS xG_per_90,
+  CASE WHEN time > 0 THEN "xA" * 90.0 / time ELSE NULL END AS xA_per_90,
+
+  goals + assists AS goal_contributions,
+
+  CASE WHEN time > 0 THEN (goals + assists) * 90.0 / time ELSE NULL END AS goal_contributions_per_90,
+
+  goals - "xG" AS xG_diff,
+  assists - "xA" AS xA_diff,
+  npg - "npxG" AS npxG_diff,
+
+  CASE 
+    WHEN "npxG"::float != 0 THEN "xG"::float / "npxG"::float 
+    ELSE NULL 
+  END AS xG_ratio,
+
+  CASE WHEN time > 0 THEN "xGChain" * 90.0 / time ELSE NULL END AS xGChain_per_90,
+  CASE WHEN time > 0 THEN "xGBuildup" * 90.0 / time ELSE NULL END AS xGBuildup_per_90,
+
+  CASE 
+    WHEN games > 0 THEN 
+      (yellow_cards * 1 + red_cards * 3)::float / games
+    ELSE NULL 
+  END AS discipline_score
 
 FROM {{ ref('players_2024') }}
