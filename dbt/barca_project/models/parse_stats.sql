699cb4d40d35f8a5a3f5712fc420691416be1a50
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2025-07-13 16:01:53 +0200

    development dbt files

--- dbt/barca_project/models/parse_stats.sql
+++ dbt/barca_project/models/parse_stats.sql
@@ -1,10 +1,8 @@
 WITH raw_data AS (
-  SELECT
-    *
+  SELECT *
   FROM {{ ref('stats_2024') }}
 ),
 
--- Ajoute un index de ligne pour déterminer les types plus tard
 with_indexed AS (
   SELECT 
     *,
@@ -12,126 +10,126 @@ with_indexed AS (
   FROM raw_data
 ),
 
--- Traitement des situations (lignes 2 à 6)
 situations AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      'Open play', 'From corner', 'Direct Freekick', 'Set piece', 'Penalty'
-    ]) AS situation_label,
+    CASE row_num
+      WHEN 1 THEN 'Open play'
+      WHEN 2 THEN 'From corner'
+      WHEN 3 THEN 'Direct Freekick'
+      WHEN 4 THEN 'Set piece'
+      WHEN 5 THEN 'Penalty'
+    END AS situation_label,
     situation AS json_data
   FROM with_indexed
-  WHERE row_num <= 5
+  WHERE row_num BETWEEN 1 AND 5
 ),
 
 -- Traitement des formations (lignes 7 à 9)
 formations AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      '4-2-3-1', '4-3-3', '4-1-4-1'
-    ]) AS formation_label,
+    CASE row_num
+      WHEN 6 THEN '4-2-3-1'
+      WHEN 7 THEN '4-3-3'
+      WHEN 8 THEN '4-1-4-1'
+    END AS formations_label,
     formation AS json_data
   FROM with_indexed
-  WHERE row_num <= 8 and row_num > 5
+  WHERE row_num BETWEEN 6 AND 8
 ),
 
 -- Traitement des États du jeu (lignes 10 à 15)
 gamestate_cte AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      'Goal diff 0', 'Goal diff +1', 'Goal diff > +1', 'Goal diff -1', 'Goal diff < -1'
-    ]) AS gameState_label,
+    CASE row_num
+      WHEN 9 THEN 'Goal diff 0'
+      WHEN 10 THEN 'Goal diff +1'
+      WHEN 11 THEN 'Goal diff > +1'
+      WHEN 12 THEN 'Goal diff -1'
+      WHEN 13 THEN 'Goal diff < -1'
+    END AS gamestats_label,
     "gameState" AS json_data
   FROM with_indexed
-  WHERE row_num <= 13 and row_num > 8
+  WHERE row_num BETWEEN 9 AND 13
 ),
 
--- Traitement des timing (lignes 15 à 21)
-timing AS (
+-- Traitement des États du jeu (lignes 10 à 15)
+timings AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      '1-15', '16-30', '31-45', '46-60', '61-75', '76+'
-    ]) AS timing_label,
+    CASE row_num
+      WHEN 14 THEN '1-15'
+      WHEN 15 THEN '16-30'
+      WHEN 16 THEN '31-45'
+      WHEN 17 THEN '46-60'
+      WHEN 18 THEN '61-75'
+      WHEN 19 THEN '76+'
+    END AS timings_label,
     timing AS json_data
   FROM with_indexed
-  WHERE row_num <= 19 and row_num > 14
+  WHERE row_num BETWEEN 14 AND 19
 ),
 
 -- Traitement des Zones de tir (lignes 22 à 25)
 Shotzones AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      'Own goals', 'Out of box', 'Penalty area', 'Six-yard box'
-    ]) AS Shotzones_label,
-    Shotzone AS json_data
+    CASE row_num
+      WHEN 20 THEN 'Own goals'
+      WHEN 21 THEN 'Out of box'
+      WHEN 22 THEN 'Penalty area'
+      WHEN 23 THEN 'Six-yard box'
+    END AS Shotzones_label,
+    "shotZone" AS json_data
   FROM with_indexed
-  WHERE row_num <= 23 and row_num > 18
+  WHERE row_num BETWEEN 20 AND 23
 ),
 
 -- Traitement des Vitesse d'attaque (lignes 26 à 29)
 Attackspeed AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      'Normal', 'Standard', 'Slow', 'Fast'
-    ]) AS Attackspeed_label,
-    Attackspeed AS json_data
+    CASE row_num
+      WHEN 24 THEN 'Normal'
+      WHEN 25 THEN 'Standard'
+      WHEN 26 THEN 'Slow'
+      WHEN 27 THEN 'Fast'
+    END AS Attackspeeds_label,
+    "attackSpeed" AS json_data
   FROM with_indexed
-  WHERE row_num <= 27 and row_num > 22
+  WHERE row_num BETWEEN 24 AND 27
 ),
 
 -- Traitement des Results (lignes 31 à 34)
 Result AS (
   SELECT
     row_num,
-    unnest(ARRAY[
-      'Blocked shot', 'Missed shot', 'Saved shot', 'Goal', 'Shot on post'
-    ]) AS Result_label,
-    Result AS json_data
+    CASE row_num
+      WHEN 28 THEN 'Shot on post'
+      WHEN 29 THEN 'Missed shot'
+      WHEN 30 THEN 'Saved shot'
+      WHEN 31 THEN 'Goal'
+      WHEN 32 THEN 'Shot on post'
+    END AS Results_label,
+    result AS json_data
   FROM with_indexed
-  WHERE row_num <= 32 and row_num > 27
+  WHERE row_num BETWEEN 28 AND 32
 ),
 
--- Parse les données de situations
-parsed_situations AS (
-  SELECT
-    situation_label,
-    CAST(json_extract_path_text(json_data::json, 'shots') AS INTEGER) AS shots,
-    CAST(json_extract_path_text(json_data::json, 'goals') AS INTEGER) AS goals,
-    CAST(json_extract_path_text(json_data::json, 'xG') AS FLOAT) AS xG,
-    CAST(json_extract_path_text(json_data::json, 'against', 'shots') AS INTEGER) AS shots_against,
-    CAST(json_extract_path_text(json_data::json, 'against', 'goals') AS INTEGER) AS goals_against,
-    CAST(json_extract_path_text(json_data::json, 'against', 'xG') AS FLOAT) AS xG_against
-  FROM situations
-),
 
--- Parse les données de formations
-parsed_formations AS (
-  SELECT
-    json_extract_path_text(json_data::json, 'stat') AS formation_label,
-    CAST(json_extract_path_text(json_data::json, 'time') AS INTEGER) AS minutes_played,
-    CAST(json_extract_path_text(json_data::json, 'shots') AS INTEGER) AS shots,
-    CAST(json_extract_path_text(json_data::json, 'goals') AS INTEGER) AS goals,
-    CAST(json_extract_path_text(json_data::json, 'xG') AS FLOAT) AS xG,
-    CAST(json_extract_path_text(json_data::json, 'against', 'shots') AS INTEGER) AS shots_against,
-    CAST(json_extract_path_text(json_data::json, 'against', 'goals') AS INTEGER) AS goals_against,
-    CAST(json_extract_path_text(json_data::json, 'against', 'xG') AS FLOAT) AS xG_against
-  FROM formations
+-- Union de tous les blocs
+all_data AS (
+  SELECT * FROM situations
+  UNION ALL SELECT * FROM formations
+  UNION ALL SELECT * FROM gamestate_cte
+  UNION ALL SELECT * FROM timings
+  UNION ALL SELECT * FROM Shotzones
+  UNION ALL SELECT * FROM Attackspeed
+  UNION ALL SELECT * FROM Result
 )
 
--- Résultat final
-SELECT * FROM parsed_situations
-UNION ALL
-SELECT 
-  formation_label AS situation_label,
-  shots,
-  goals,
-  xG,
-  shots_against,
-  goals_against,
-  xG_against
-FROM parsed_formations
+
+SELECT * FROM all_data
+
