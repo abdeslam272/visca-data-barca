4e2cfe684dc7e595dcb2597070564382ecb2334a
abdeslam272 <abdeslam1999elbennouri@gmail.com>
2025-12-14 15:47:51 +0100

    development dbt files

--- dbt/barca_project/models/staging/raw/base_players.sql
+++ dbt/barca_project/models/staging/raw/base_players.sql
@@ -2,25 +2,61 @@
 
 with
 {% for year in years %}
-raw_data_{{year}} as (
-  select *
-  from {{ ref('players_' ~ year) }}
+
+raw_data_{{ year }} as (
+    select *
+    from {{ ref('players_' ~ year) }}
 ),
 
-with_indexed_{{year}} as (
-  select
-    {{year}} as year,,
-    player_name,
-    games as games_played,
-    time as minutes_played,
-    goals as goals_scored,
-    xG as xG_created,
-    assists,
-    xA as assists_created,
-    shots,
-    key_passes,
-    npg as non_penalty_goals,
-    xGchain, --xGChain gives every player who touches the ball in a possession the shot's xG if that possession ends in a shot
-    xGBuildup, --based on knowing which players are part of passing chains that end in a shot but without taking into account the last two links - shot and last pass
-    CASE WHEN xG = 0 then null else goals/xG end as efficient_goal_scoring,
-    ),
+cleaned_players_{{ year }} as (
+    select
+        {{ year }} as year,
+        player_name,
+        games as games_played,
+        time as minutes_played,
+        goals as goals_scored,
+
+        -- xG metrics (cast once)
+        cast("xG" as float)        as xg_created,
+        cast("xA" as float)        as assists_created,
+        cast("xGChain" as float)   as xg_chain,
+        cast("xGBuildup" as float) as xg_buildup,
+
+        assists,
+        shots,
+        key_passes,
+        npg as non_penalty_goals
+
+    from raw_data_{{ year }}
+),
+
+transformed_players_{{ year }} as (
+    select
+        year,
+        player_name,
+        games_played,
+        minutes_played,
+        goals_scored,
+        xg_created,
+        assists,
+        assists_created,
+        shots,
+        key_passes,
+        non_penalty_goals,
+        xg_chain,
+        xg_buildup,
+
+        goals_scored / nullif(xg_created, 0) as efficient_goal_scoring
+
+    from cleaned_players_{{ year }}
+)
+
+{% if not loop.last %},{% endif %}
+{% endfor %}
+
+-- union final
+select * from transformed_players_{{ years[0] }}
+{% for year in years[1:] %}
+union all
+select * from transformed_players_{{ year }}
+{% endfor %}
